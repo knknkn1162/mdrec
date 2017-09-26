d1418179caee57edf348fc01086ce92d7309e195
knknkn1162 <knknkn1162@gmail.com>
2017-09-26 11:14:26 +0900

    test MDRec class

---
+++ tests/mdrec_test.py
@@ -0,0 +1,32 @@
+import unittest
+from pandas import DataFrame
+from mdrec import MDRec
+
+class MDRecTests(unittest.TestCase):
+
+    def test_rec(self):
+
+        save_file = "./out/test.md"
+        r = MDRec(save_file=save_file)
+
+        res = r.rec("test", h=1)
+        newline = "\n\n"
+        self.assertEqual(res, "# test"+newline)
+
+        msg = "teststeststests"
+        res = r.rec(msg)
+        self.assertEqual(res, msg + newline)
+
+        res = r.rec(["bbbi", "ddn", 3])
+        self.assertEqual(res, "+ bbbi{}+ ddn{}+ 3{}".format(newline, newline, newline))
+
+        r.img2md(src="./out/img/test01.png")
+        # display_test
+        df = DataFrame([[12, 2, 4, 3], [3, 3, 3, 4]], columns=list("abcd"), index=["AB", "BB"])
+        r.rec(df, title="sample")
+
+        r.to_html()
+
+
+if __name__ == "__main__":
+    unittest.main()
