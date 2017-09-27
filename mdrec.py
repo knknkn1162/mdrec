6bb04544452cb44bd688a62ec5b13688ba12056e
knknkn1162 <knknkn1162@gmail.com>
2017-09-27 15:21:21 +0900

    separate io logic and text logic

--- mdrec.py
+++ mdrec.py
@@ -8,20 +8,16 @@ from IPython.display import display_markdown  # show とか、 record_md など�
 
 from collections.abc import Iterable
 import logging
-import shutil
+import shutil, uuid
 
 logger = logging.getLogger(__name__)
 
 
 class MDRec():
     def __init__(self, *, save_file=None, refresh=True):
-        self.path = Path(save_file) if save_file is not None else Path('./md/test.md')
+        self.path = Path(save_file) if save_file is not None else Path('./md/{}.md'.format(uuid.uuid4()))
         self.refresh = refresh
         self.counter = 0
-
-    @staticmethod
-    def _append_new_line(data):
-        return data + "\n"
 
     """
     depending on type of data argument, change output.
@@ -34,42 +30,29 @@ class MDRec():
     def rec(self, data, *, h=None, title=None, display_notebook=True, numbering=False):
         res = ""
         if any(list(map(lambda t: isinstance(data, t), [DataFrame, Series]))):
-            res += self.df2md(data, h=h, title=title)
+            res += self.table(data, h=h, title=title)
         elif isinstance(data, str):
-            hmapping = ["#" * i + " "*(i>0) for i in range(7)]
-            res += self._append_new_line("{}{}".format(hmapping[h or 0], data))
+            self.heading(data, h=h)
         elif isinstance(data, Iterable):  # except type of str
             res += self.enum(data, numbering)
         else:
-            res += self._append_new_line(str(data))
+            res += self.heading(data, h=h)
 
         if display_notebook:
             display_markdown(res, raw=True)
 
         return self._save(res)
 
-    """
-    if numbering:
-    1. foo
-    2. hoge
-    3. ...
+    """insert image in markdown file"""
+    def rec_img(self, src, *, alt=None, title=None):
+        return self._save(self.img(src, alt=alt, title=title))
 
-    else:
-    + foo
-    + hoge
-    + ...
-    """
-    def enum(self, lst, numbering=False):
-        sign = "1." if numbering else "+"
-        res = []
-        for elem in lst:
-            res.append(
-                self._append_new_line("{} {}".format(sign, elem))
-            )
+    """convert markdown file to html"""
+    def to_html(self, *, render_inline=True, title=None):
+        return grip.export(self.path, title=title, render_inline=render_inline)
 
-        return "".join(res)
-
-    def img2md(self, src, *, alt=None, title=None):
+    """generate markdown formatted img expression"""
+    def img(self, src, *, alt=None, title=None):
         src_path = Path(src)
         stem = src_path.stem
         parent = self.path.parent
@@ -85,13 +68,34 @@ class MDRec():
         title = title or stem
 
         res = '''![{}]({} "{}")'''.format(alt, dst_img_path, title)
-        return self._save(self._append_new_line(res))
+        return self._end(res)
+
+    @staticmethod
+    def _end(data):
+        return data + "\n\n"
+
+    @staticmethod
+    def heading(data, h):
+        hmapping = ["#" * i + " " * (i > 0) for i in range(7)]
+        res = "{}{}".format(hmapping[h or 0], str(data))
+        return MDRec._end(res)
+
+    """
+    switch itemize or enumerate with numbering argument
+    """
+    @staticmethod
+    def enum(lst, numbering=False):
+        mark = "1." if numbering else "+"
+        res = []
+        for elem in lst:
+            res.append(
+                "{} {}".format(mark, elem))
+
+        return MDRec._end("\n".join(res))
 
     """convert dataframe to markdown using pytablewriter module"""
-    def df2md(self, df, *,
-              h=None, title=None):
-
-        if h is None: h = 2
+    @staticmethod
+    def table(df, *, h=2, title=None):
         if isinstance(df, Series):
             df = DataFrame(df)
 
@@ -108,8 +112,8 @@ class MDRec():
         # fitted multiple cases
         writer.header_list = ["  {}  ".format(a) for a in df.columns]
 
-        res = self._logging(writer)
-        return res
+        res = MDRec._write(writer)
+        return res + "\n"
 
     def _save(self, res):
         mode = "w" if (self.counter == 0 and self.refresh == True) else "a"
@@ -118,15 +122,9 @@ class MDRec():
         self.counter += 1
         return res
 
-    def _to_data_url(self, url, type):
-        pass
-
-    def _logging(self, writer):
+    @staticmethod
+    def _write(writer):
         writer.stream = six.StringIO()
         writer.write_table()
         return writer.stream.getvalue()
-
-    """convert markdown file to html"""
-    def to_html(self, *, render_inline=True, title=None):
-        grip.export(self.path, title=title, render_inline=render_inline)
 
