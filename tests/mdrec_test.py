8ddf5fd51cb63776bd18cdee68c37b190b1c75be
knknkn1162 <knknkn1162@gmail.com>
2017-09-28 13:37:10 +0900

    test in img & rec_img

--- tests/mdrec_test.py
+++ tests/mdrec_test.py
@@ -13,23 +13,33 @@ class MDRecTests(unittest.TestCase):
         self.assertEqual(res, "1. 1\n1. 2\n1. 3\n\n")
 
     def test_img(self):
-        save_file = "./out/test.md"
+        save_file = "./out/test_img.md"
         r = MDRec(save_file=save_file)
-        res = r.img(src="./out/img/test01.png", title="test", alt="sample")
+        res = r.img(src="./out/src/test01.png", title="test", alt="sample")
         # Note that this writes file in the save_file path
         self.assertEqual(res, '''![sample](img/test01.png "test")\n\n''')
 
-    def test_img2(self):
-        save_file = "./out/test.md"
+
+    def test_img_with_ignore(self):
+        save_file = "./out/test_ignore.md"
         r = MDRec(save_file=save_file)
-        res = r.img(src="../test02.png", title="test", alt="sample")
+        res = r.rec_img(src="./dummy/tes.png", title="test", alt="sample", ignore=True)
         # Note that this writes file in the save_file path
-        self.assertEqual(res, '''![sample](img/test02.png "test")\n\n''')
+        self.assertEqual(res, '''![sample](img/tes.png "test")\n\n''')
+
 
     def test_table(self):
         df = DataFrame([[12, 2, 4, 3], [3, 3, 3, 4]], columns=list("abcd"), index=["AB", "BB"])
         res = MDRec.table(df, title="sample")
         self.assertEqual(res, df_md_sample)
+
+    def test_non_name(self):
+        r = MDRec()
+        res = r.rec_img(src="./out/src/test02.png", title="test", alt="sample")
+        # Note that this writes file in the save_file path
+        self.assertEqual(res, '''![sample](img/test02.png "test")\n\n''')
+
+
 
 df_md_sample = \
 """## sample
