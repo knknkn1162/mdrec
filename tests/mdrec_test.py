3e6e1a37a3d8695fe72f0730432c9523cb4ebb83
knknkn1162 <knknkn1162@gmail.com>
2017-09-29 15:48:26 +0900

    test MDRec

--- tests/mdrec_test.py
+++ tests/mdrec_test.py
@@ -17,15 +17,22 @@ class MDRecTests(unittest.TestCase):
         self.assertEqual(res, ja_sample)
 
         res = r.rec([1,[1,2],[2,[3],4], [{"a": 100, "b": 234}], {"c" : [3,4,5]}])
-
         self.assertEqual(res, nested_sample)
+
+        res = r.link("../src/tes.png", text="testtest", img=False)
+        self.assertEqual(res, "[testtest](../../src/tes.png)\n\n")
 
 
     def test_img(self):
         save_file = "./out/test_img.md"
         r = MDRec(save_file=save_file)
-        r.img(src="./out/src/test01.png", title="test", alt="sample", ignore=True)
+        try:
+            r.img(src="./test01.png", title="test", text="sample")
+        except:
+            self.assertRaises(IOError)
 
+        res = r.img(src="./out/src/test01.png", title="test", text="sample", ignore=True)
+        self.assertEqual(res, '''![sample](img/test01.png "test")\n\n''')
         r.to_html()
 
         self.assertTrue(os.path.exists("./out/test_img.html"))
