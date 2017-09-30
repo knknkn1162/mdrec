3ffe9309ba8790f98eea0ddf46315c4de0955693
knknkn1162 <knknkn1162@gmail.com>
2017-09-30 23:51:19 +0900

    modify find_packages in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = [
 setup(name='mdrec',
       version='0.1dev',
       description='record python object in markdown file & convert to html.',
-      packages=find_packages("mdrec"),
+      packages=find_packages(exclude=["tests"]),
       include_package_data=True,
       zip_safe=False,
       install_requires=install_requires,
