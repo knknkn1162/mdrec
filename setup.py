65cc5646ef9651a94088ee8f56f761fb60c90aa1
knknkn1162 <knknkn1162@gmail.com>
2017-10-01 10:17:36 +0900

    add test_suite in setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,14 @@ install_requires = [
 setup(name='mdrec',
       version='0.1dev',
       description='record python object in markdown file & convert to html.',
+      classifiers=[
+          "Programming Language :: Python :: 3 :: Only",
+          "Framework:: IPython",
+      ],
       packages=find_packages(exclude=["tests"]),
       include_package_data=True,
       zip_safe=False,
+      test_suite="tests",
       install_requires=install_requires,
       entry_points="""
 """)
